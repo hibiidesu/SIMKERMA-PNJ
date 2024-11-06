207bfb2401f76c8e30dade7f209e16ee3b61778e
Schutz3 <02.farhan.dwi@gmail.com>
2024-11-07 02:56:19 +0700

    Fix: Anjay nyepam push

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,19 +9,19 @@ sleep 30
 FIRST_RUN_FILE="/var/www/html/.first_run_completed"
 
 if [ ! -f "$FIRST_RUN_FILE" ]; then
-    echo "First run! memulai seeding dan migrate"
-    php artisan migrate --force || { echo "Migration gagal"; exit 1; }
-    php artisan db:seed --force || { echo "Seeding gagal"; exit 1; }
-    touch "$FIRST_RUN_FILE"
-
-    echo "Selesai"
-
+    echo "First run! memulai install composer, seeding dan migrate"
     cd /var/www
     composer install --no-dev --optimize-autoloader
 
     chown -R www-data:www-data /var/www
     chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
+
+    php artisan migrate --force || { echo "Migration gagal"; exit 1; }
+    php artisan db:seed --force || { echo "Seeding gagal"; exit 1; }
+    touch "$FIRST_RUN_FILE"
+
+    echo "Selesai"
 
 else
     echo "Bukan run pertama kali"
