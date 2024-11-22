02b03ff65eed43efcc8cbbbee530ea7ed5a200a4
Schutz3 <02.farhan.dwi@gmail.com>
2024-11-22 22:34:06 +0700

    Fix: Revert SSOController

--- app/Http/Controllers/Auth/SSOController.php
+++ app/Http/Controllers/Auth/SSOController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Carbon;
 
-class ssocontroller extends Controller
+class SSOController extends Controller
 {
     // tinggal di rubah menggunakan SSO PNJ
     public function loginSSO()
