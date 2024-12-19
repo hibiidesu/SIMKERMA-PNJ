495922c56aa4f70137d04fcbd9ff80dbe0f508f6
Schutz3 <02.farhan.dwi@gmail.com>
2024-12-20 04:03:47 +0700

    Fix: Tracker API. Seeder

--- app/Http/Controllers/v1/trackerApi.php
+++ app/Http/Controllers/v1/trackerApi.php
@@ -37,65 +37,61 @@ class trackerApi extends Controller
     }
     public function getKerjasama($id)
     {
-        $data = Kerjasama::with([
-            'log_persetujuan',
-        ])
-            ->where('id', $id)
-            ->get();
+        $kerjasama = Kerjasama::with(['log_persetujuan'])->find($id);
 
-        if ($data->isEmpty()) {
+        if (!$kerjasama) {
             return response()->json([
                 'message' => 'error'
             ], 404);
         }
 
+        $kriteriaMitraIds = explode(',', $kerjasama->kriteria_mitra_id);
+        $kriteriaKemitraanIds = json_decode($kerjasama->kriteria_kemitraan_id);
+        $kriteriaMitra = kriteria_mitra::whereIn('id', $kriteriaMitraIds)->get();
+        $kriteriaKemitraan = kriteria_kemitraan::whereIn('id', $kriteriaKemitraanIds)->get();
+        $pksIds = explode(',', $kerjasama->pks);
+        $pks = pks::whereIn('id', $pksIds)->get();
+        $jurusanID = explode(',', $kerjasama->jurusan);
+        $jurusan = Unit::whereIn('id', $jurusanID)->get();
+        $prodiID = explode(',', $kerjasama->prodi);
+        $prodi = prodi::whereIn('id', $prodiID)->get();
+
+        $responseData = [
+            'id' => $kerjasama->id,
+            'kerjasama' => $kerjasama->kerjasama,
+            'mitra' => $kerjasama->mitra,
+            'tanggal_mulai' => $kerjasama->tanggal_mulai,
+            'tanggal_selesai' => $kerjasama->tanggal_selesai,
+            'nomor' => $kerjasama->nomor,
+            'kegiatan' => $kerjasama->kegiatan,
+            'jenis_kerjasama' => $kerjasama->jenis_kerjasama->jenis_kerjasama,
+            'kriteria_mitra' => $kriteriaMitra->pluck('kriteria_mitra'),
+            'kriteria_kemitraan' => $kriteriaKemitraan->pluck('kriteria_kemitraan'),
+            'sifat' => $kerjasama->sifat,
+            'pks' => $pks->pluck('pks'),
+            'jurusan' => $jurusan->pluck('name'),
+            'prodi' => $prodi->pluck('name'),
+            'pic_pnj' => $kerjasama->pic_pnj,
+            'alamat_perusahaan' => $kerjasama->alamat_perusahaan,
+            'pic_industri' => $kerjasama->pic_industri,
+            'jabatan_pic_industri' => $kerjasama->jabatan_pic_industri,
+            'telp_industri' => $kerjasama->telp_industri,
+            'catatan' => $kerjasama->catatan,
+            'email' => $kerjasama->email,
+            'step_code' => $kerjasama->step,
+            'step' => $this->getStepData($kerjasama->step),
+            'log' => $kerjasama->log_persetujuan->map(function ($log, $index) {
+                return [
+                    'index' => $index + 1,
+                    'created_at' => $log->created_at->format('d-m-Y H:i:s'),
+                    'step' => $log->getStep() . ' Oleh ' . $log->user->name . ' (' . $log->user->role->role_name . ')'
+                ];
+            }),
+        ];
+
         return response()->json([
             'message' => 'success',
-            'data' => $data->map(function ($kerjasama) {
-                $kriteriaMitraIds = explode(',', $kerjasama->kriteria_mitra_id);
-                $kriteriaKemitraanIds = explode(',', $kerjasama->kriteria_kemitraan_id);
-                $kriteriaMitra = kriteria_mitra::whereIn('id', $kriteriaMitraIds)->get();
-                $kriteriaKemitraan = kriteria_kemitraan::whereIn('id', $kriteriaKemitraanIds)->get();
-                $pksIds = explode(',', $kerjasama->pks);
-                $pks = pks::whereIn('id', $pksIds)->get();
-                $jurusanID = explode(',', $kerjasama->jurusan);
-                $jurusan = Unit::whereIn('id',$jurusanID)->get();
-                $prodiID = explode(',', $kerjasama->prodi);
-                $prodi = prodi::whereIn('id',$prodiID)->get();
-                return [
-                    'id' => $kerjasama->id,
-                    'kerjasama' => $kerjasama->kerjasama,
-                    'mitra' => $kerjasama->mitra,
-                    'tanggal_mulai' => $kerjasama->tanggal_mulai,
-                    'tanggal_selesai' => $kerjasama->tanggal_selesai,
-                    'nomor' => $kerjasama->nomor,
-                    'kegiatan' => $kerjasama->kegiatan,
-                    'jenis_kerjasama' => $kerjasama->jenis_kerjasama->jenis_kerjasama,
-                    'kriteria_mitra' => $kriteriaMitra->pluck('kriteria_mitra'),
-                    'kriteria_kemitraan' => $kriteriaKemitraan->pluck('kriteria_kemitraan'),
-                    'sifat' => $kerjasama->sifat,
-                    'pks' => $pks->pluck('pks'),
-                    'jurusan' => $jurusan->pluck('name'),
-                    'prodi' => $prodi->pluck('name'),
-                    'pic_pnj' => $kerjasama->pic_pnj,
-                    'alamat_perusahaan' => $kerjasama->alamat_perusahaan,
-                    'pic_industri' => $kerjasama->pic_industri,
-                    'jabatan_pic_industri' => $kerjasama->jabatan_pic_industri,
-                    'telp_industri' => $kerjasama->telp_industri,
-                    'catatan' => $kerjasama->catatan,
-                    'email' => $kerjasama->email,
-#                    'file' => asset('surat_kerjasama/' . $kerjasama->file),
-                    'step_code' => $kerjasama->step,
-                    'step' => $this->getStepData($kerjasama->step),
-                    'log' => $kerjasama->log_persetujuan->map(function ($log, $index) {
-                        return [
-                            'index' => $index + 1,
-                            'created_at' => $log->created_at->format('d-m-Y H:i:s'),
-                            'step' => $log->getStep() . ' Oleh ' . $log->user->name . ' (' . $log->user->role->role_name . ')'
-                        ];
-                    }),
-                ];
-            })
+            'data' => $responseData
         ]);
     }
 
